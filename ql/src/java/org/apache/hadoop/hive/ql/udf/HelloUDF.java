376a39963d7b482b1bab11289fd85d187116e4c5
charliewu <1075318919@qq.com>
2021-03-08 23:24:18 +0800

    bug fix

--- ql/src/java/org/apache/hadoop/hive/ql/udf/HelloUDF.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/HelloUDF.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.io.Text;
  * @Date: 2021/3/8 23:01
  * @Description: 自定义Hive UDF函数
  */
-@Description(name = "HelloUDF,
+@Description(name = "HelloUDF",
         value = "_FUNC_(input_str) - returns Hello : input_str ",
         extended =  "Example:\n "
                 + "  > SELECT _FUNC_('moyuan') FROM src LIMIT 1;\n"
@@ -17,5 +17,11 @@ import org.apache.hadoop.io.Text;
 public class HelloUDF extends UDF {
     public Text evaluate(Text input){
         return new Text("Hello: "+input);
+    }
+
+    // 测试代码
+    public static void main(String[] args) {
+        HelloUDF udf = new HelloUDF();
+        System.out.println(udf.evaluate(new Text("Charlie")));
     }
 }
